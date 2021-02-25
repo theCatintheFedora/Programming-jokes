3a44afc430fdc85ec6b3a0f3979eaac0e4bd7802
Timothy Price <theCatintheFedora1367@gmail.com>
2021-02-25 10:46:34 -0600

    got API key

--- Programming jokes/ContentView.swift
+++ Programming jokes/ContentView.swift
@@ -26,12 +26,7 @@ struct ContentView: View {
         })
     }
     func getJokes() {
-        jokes.append(Joke(setup: "Why do programmers always get Christmas and Halloween mixed up?",
-                          punchline: "Because DEC 25 = OCT 31"))
-        jokes.append(Joke(setup: "How did the programmer die in the shower?",
-                          punchline: "She followed the shampoo bottle instructions: Lather. Rinse. Repeat."))
-        jokes.append(Joke(setup: "There are 10 types of people in the world",
-                          punchline: "Those who understand binary and those who don’t."))
+        let apiKey = "?rapidapi-key=ba1b549956msha6fa1902ee76913p1104c9jsncc2a41204a3d"
     }
 }
 
