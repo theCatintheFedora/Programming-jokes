35c02f6451e9f188cb7aaa623030ed0ace7c2fd5
Timothy Price <theCatintheFedora1367@gmail.com>
2021-02-25 10:38:05 -0600

    my code is working perfectly with no errors... something is wrong here

--- Programming jokes/ContentView.swift
+++ Programming jokes/ContentView.swift
@@ -8,10 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var jokes = [Joke]()
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List(jokes) { joke in
+                NavigationLink(
+                    destination: Text(joke.punchline)
+                        .padding(),
+                    label: {
+                        Text(joke.setup)
+                    })
+            }
+            .navigationTitle("Programming Jokes")
+        }
+        .onAppear(perform: {
+            getJokes()
+        })
     }
+    func getJokes() {
+        jokes.append(Joke(setup: "Why do programmers always get Christmas and Halloween mixed up?",
+                          punchline: "Because DEC 25 = OCT 31"))
+        jokes.append(Joke(setup: "How did the programmer die in the shower?",
+                          punchline: "She followed the shampoo bottle instructions: Lather. Rinse. Repeat."))
+        jokes.append(Joke(setup: "There are 10 types of people in the world",
+                          punchline: "Those who understand binary and those who don’t."))
+    }
+}
+
+struct Joke: Identifiable {
+    let id = UUID()
+    var setup: String
+    var punchline: String
 }
 
 struct ContentView_Previews: PreviewProvider {
