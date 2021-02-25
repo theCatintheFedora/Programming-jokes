0f5f46026ad8905bc557cfc5b8d72aaf72f45e18
Timothy Price <theCatintheFedora1367@gmail.com>
2021-02-25 10:24:59 -0600

    Initial Commit

---
+++ Programming jokes/Programming_jokesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Programming_jokesApp.swift
+//  Programming jokes
+//
+//  Created by Student on 2/25/21.
+//
+
+import SwiftUI
+
+@main
+struct Programming_jokesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
